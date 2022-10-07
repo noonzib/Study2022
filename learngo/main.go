f4cfb130b6d0fb995dbaecdfc7e44c97e3b56067
noonzib <noonzib14@gmail.com>
2022-10-07 17:48:20 +0900

    Slow URL Checker

--- learngo/main.go
+++ learngo/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
 
+var errRequestFailed = errors.New("request failed")
+
+func main() {
+	var results = make(map[string]string)
+
+	urls := []string{
+		"https://github.com/noonzib/Study2022",
+		"https://www.google.com/",
+		"https://www.youtube.com/",
+		"https://www.apple.com/",
+		"https://www.naver.com/",
+		"https://nomadcoders.co/",
+	}
+
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+
+	}
+	return nil
 }
